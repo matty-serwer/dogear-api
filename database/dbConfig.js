3c9255eaf5ed9106097e5065a7e8b2f24a13c423
Matthew Serwer <matthewserwer@matthews-air.myfiosgateway.com>
2022-04-14 22:09:15 -0400

    Creates user router and user model for testing database. POST user and GET users.

---
+++ database/dbConfig.js
@@ -0,0 +1,8 @@
+const knex = require("knex");
+
+const env = process.env.DB_ENV || "development";
+const config = require("../knexfile.js");
+
+const db = knex(config[env]);
+
+module.exports = db;
---
+++ knexfile.js
@@ -0,0 +1,36 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      database: "dogear_db",
+      user: "postgres",
+      password: "Password!",
+    },
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+  },
+};
