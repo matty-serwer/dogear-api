c472358c9ed63c9af8fc1ee9ea52868fbb66abd2
Matthew Serwer <matthewserwer@matthews-air.myfiosgateway.com>
2022-04-26 21:33:21 -0400

    Adds user, club, post, and relationships migrations

--- database/migrations/20220414184903_user.js
+++ database/migrations/20220414184903_user.js
@@ -6,15 +6,15 @@
 exports.up = function (knex) {
   return knex.schema.createTable("users", (table) => {
     table.string("id").primary().unique();
-    table.string("username").unique();
-    table.string("password");
-    table.string("email").unique();
+    table.string("username").unique().notNullable();
+    table.string("password").notNullable();
+    table.string("email").unique().notNullable();
     table.string("first_name");
     table.string("last_name");
-    table.integer("num_clubs");
-    table.integer("num_clubs_organized");
-    table.integer("points");
-    table.integer("books_completed");
+    table.integer("num_clubs").defaultTo(0);
+    table.integer("num_clubs_organized").defaultTo(0);
+    table.integer("points").unsigned().defaultTo(0);
+    table.integer("books_completed").defaultTo(0);
     table.timestamps(true, true);
   });
 };
---
+++ database/migrations/20220427001251_relationships.js
@@ -0,0 +1,100 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("liked_posts", (table) => {
+      table.increments();
+      table
+        .string("user_id")
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE")
+        .notNullable();
+      table
+        .integer("post_id")
+        .unsigned()
+        .references("id")
+        .inTable("posts")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE")
+        .notNullable();
+      table.timestamps(true, true);
+    })
+    .createTable("liked_comments", (table) => {
+      table.increments();
+      table
+        .string("user_id")
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE")
+        .notNullable();
+      table
+        .integer("comment_id")
+        .unsigned()
+        .references("id")
+        .inTable("comments")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE")
+        .notNullable();
+      table.timestamps(true, true);
+    })
+    .createTable("user_to_club", (table) => {
+      table.increments();
+      table
+        .string("user_id")
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE")
+        .notNullable();
+      table
+        .integer("club_id")
+        .unsigned()
+        .references("id")
+        .inTable("clubs")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE")
+        .notNullable();
+      table.timestamps(true, true);
+    })
+    .createTable("user_checkpoint", (table) => {
+      table.increments();
+      table
+        .string("user_id")
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE")
+        .notNullable();
+      table
+        .integer("book_instance_id")
+        .unsigned()
+        .references("id")
+        .inTable("book_instance")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE")
+        .notNullable();
+      table.integer("checkpoint");
+      table.timestamps(true, true);
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema
+    .dropTable("user_checkpoint")
+    .dropTable("user_to_club")
+    .dropTable("liked_comments")
+    .dropTable("liked_posts");
+};
